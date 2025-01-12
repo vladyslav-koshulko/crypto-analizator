5387e9d0222548ac1d0cc6d9d3d43f1f280f420a
vladyslav <koshulkov@gmail.com>
2025-01-12 17:39:26 +0200

    Updated main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import crypto.ArgsAnalyzer;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        ArgsAnalyzer.getDefault(args).analyze();
     }
 }
