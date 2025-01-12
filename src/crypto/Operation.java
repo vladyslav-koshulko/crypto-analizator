7e4020edd71d12c8caafb8b1360cf9c7b1952617
vladyslav <koshulkov@gmail.com>
2025-01-12 17:39:00 +0200

    Added enum operations

---
+++ src/crypto/Operation.java
@@ -0,0 +1,17 @@
+package crypto;
+
+public enum Operation {
+    ENCRYPT("ENCRYPT"),
+    DECRYPT("DECRYPT"),
+    ANALYZE("ANALYZE"),;
+
+    private final String operation;
+
+    Operation(String operation) {
+        this.operation = operation;
+    }
+
+    public Operation getOperation() {
+        return this;
+    }
+}
